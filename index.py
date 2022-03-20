9669b4354d8287889f9a586be2cdae2466c1c3d6
ASUS <anonimus.pochti@gmail.com>
2022-03-20 20:43:56 +0200

    adminka

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 
 import json
+import traceback
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///baza.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 
@@ -18,9 +22,10 @@ class Categories(db.Model):
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    price = db.Column(db.Integer)
-    weight = db.Column(db.String)
-    description = db.Column(db.String)
+    price = db.Column(db.String, nullable=False)
+    weight = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    filename = db.Column(db.String, default="-1.png")
 
 
 @app.route("/")
@@ -48,6 +53,70 @@ def show_meal(food_id):
 @app.route("/sales")
 def open_sales():
     return render_template("sales.html")
+
+
+@app.route("/admin")
+def open_admin():
+    base = []
+    all_categories = db.session.query(Categories).all()
+    for category in all_categories:
+        foods_ids = json.loads(category.food_list)
+        food_list = db.session.query(Food).filter(Food.id.in_(foods_ids)).all()
+
+        base.append([category.name, food_list])
+
+    return render_template("admin.html", base=base)
+
+
+@app.route("/save_changes", methods=["POST"])
+def save_changes():
+    if request.method == 'POST':
+
+        try:
+            foods_ids = db.session.query(Food.id).all()
+
+            for index in foods_ids:
+                food = db.session.query(Food).filter_by(id=index[0]).first_or_404()
+
+                name_temp = request.form.get("name_" + str(index[0]))
+                price_temp = request.form.get("price_" + str(index[0]))
+                weight_temp = request.form.get("weight_" + str(index[0]))
+                description_temp = request.form.get("description_" + str(index[0]))
+
+                file = request.files["file_" + str(index[0])]
+
+                if file and file.filename != '' and '.' in file.filename:
+                    name = secure_filename(file.filename).split(".")
+                    if name[1].lower() in ['jpg', 'jpeg', 'png']:
+                        endgame = str(index[0]-1) + "." + name[1]
+                        file.save(os.path.join("static/media/food", endgame))
+                        food.filename = endgame
+                        db.session.commit()
+
+                if food.name != name_temp or food.price != price_temp or food.weight != weight_temp \
+                        or food.description != description_temp:
+
+                    if food.name != name_temp:
+                        food.name = name_temp
+
+                    if food.price != price_temp:
+                        food.price = price_temp
+
+                    if food.weight != weight_temp:
+                        food.weight = weight_temp
+
+                    if food.description != description_temp:
+                        food.description = description_temp
+
+                    db.session.commit()
+
+        except Exception as e:
+            print(str(e))
+            print(traceback.format_exc())
+
+        return redirect("/admin")
+    else:
+        return 1
 
 
 @app.route("/favicon.ico")
